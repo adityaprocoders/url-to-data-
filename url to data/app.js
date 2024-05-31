bf846d965d29482b167b4134fa4247315847ebef
Aditya Kumar Singh <168503964+adityaprocoders@users.noreply.github.com>
2024-05-30 22:22:29 -1200

    Add files via upload

---
+++ url to data/app.js
@@ -0,0 +1,17 @@
+const urlInput = document.querySelector("input");
+const downloadBtn = document.querySelector("button");
+
+downloadBtn.addEventListener("click", async ()=> {
+    try{
+        const response = await fetch(urlInput.value);
+        const file = await response.blob();
+        const link = document.createElement("a");
+        link.href = URL.createObjectURL(file);
+        link.download = new Date().getTime();
+        link.click();
+    }catch(e){
+        alert("Failed to download the file")
+
+    }
+})
+ 
